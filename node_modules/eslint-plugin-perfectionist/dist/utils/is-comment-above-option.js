7db6db56e9b125e512cfe9df43641d7b825bc008
yeon-11 <wldus4660@naver.com>
2025-09-16 18:17:42 +0900

    Delete node_modules directory

--- node_modules/eslint-plugin-perfectionist/dist/utils/is-comment-above-option.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' })
-let isCommentAboveOption = groupOption =>
-  typeof groupOption === 'object' && 'commentAbove' in groupOption
-exports.isCommentAboveOption = isCommentAboveOption
--- node_modules/eslint-plugin-perfectionist/dist/utils/unreachable-case-error.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' })
-class UnreachableCaseError extends Error {
-  constructor(value) {
-    super(`Unreachable case: ${value}`)
-    this.name = 'UnreachableCaseError'
-  }
-}
-exports.UnreachableCaseError = UnreachableCaseError
